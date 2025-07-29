734ec4ab9f77576fcc57649a8eabc96006e4ab50
Nilesh Yadav <nileshyadav667@gmail.com>
2025-07-29 10:48:39 +0530

    adding RG and Storage Account from bareilly

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "azurerm_resource_group" "rgNew1" {
+    name = "RgNilesh501"
+    location = "Central India"
+}
+
+resource "azurerm_resource_group" "rgNew2" {
+    name = "RgNilesh502"
+    location = "Central India"
+}
