2f5829ff08b9a0357646660b874867a9f692a504
Nilesh Yadav <nileshyadav667@gmail.com>
2025-07-29 11:19:34 +0530

    Adding one more RG504  From Lucknow

--- main.tf
+++ main.tf
@@ -7,3 +7,8 @@ resource "azurerm_resource_group" "rgNew2" {
     name = "RgNilesh502"
     location = "Central India"
 }
+
+resource "azurerm_resource_group" "rgNew4" {
+    name = "RgNilesh504"
+    location = "Central India"
+}
